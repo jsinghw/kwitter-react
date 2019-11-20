f125617d6eac36614885af57da83f8df613b927a
Cory <CoryTraxler@iCloud.com>
2019-11-20 11:37:23 -0500

    Forgot to remove the components from the profile.js component

--- src/react/pages/Profile.js
+++ src/react/pages/Profile.js
@@ -14,11 +14,7 @@ class Profile extends React.Component {
       <>
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
-        <ListOfUsers/>
-
         <EditProfile />
-
-        <KweetCard/>
       </>
     );
   }
