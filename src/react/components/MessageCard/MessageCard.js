ce1669511e5d7fde44d1fe294876285ea7cc38d2
Chelsea <chewh115@gmail.com>
2019-11-21 12:25:32 -0500

    Updates Message Card to include brief conversation, timestamps and send message input field along with icons.

--- src/react/components/MessageCard/MessageCard.js
+++ src/react/components/MessageCard/MessageCard.js
@@ -2,19 +2,28 @@ import React from "react";
 import { Card, Avatar, Icon } from "antd";
 import "antd/dist/antd.css";
 import "./MessageCard.css"
+import TextArea from "antd/lib/input/TextArea";
 
 class MessageCard extends React.Component {
     render() {
         return(
-            <React.Fragment>
-                <div>
-                    <h3>Message with person</h3>
-                    <Icon type="delete" />
+            <Card id="individualMessage" size="default" style={{width:"500px"}} title="Message with testing-account">
+                <Icon type="delete" />
+                <div className="messageFrom">
+                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                    <span>Lorem ipsum dolor amet bicycle rights YOLO craft beer, hashtag plaid pug asymmetrical activated charcoal. Jianbing vape venmo, subway tile la croix small batch typewriter. </span>
+                    <p>Nov 21, 2019, 11:26 AM</p>
                 </div>
-                <div>
-
+                <div className="messageTo">
+                    <span>Enamel pin snackwave godard cred vexillologist, single-origin coffee brooklyn hell of fixie letterpress etsy stumptown asymmetrical keffiyeh cray.</span>
+                    <p>Nov 21, 2019, 12:10 PM</p>
                 </div>
-            </React.Fragment>
+                <footer>
+                    <TextArea rows={1} style={{width:"300px"}}placeholder="Send new message" />
+                    <Icon style={{padding:"0.5em"}} type="mail" />
+                </footer>
+                
+            </Card>
         )
     }
 }
