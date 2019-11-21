bc3d46ca61d427ea62aa3eaee232f5dbf8c63864
Cory <CoryTraxler@iCloud.com>
2019-11-21 14:14:13 -0500

    Updated edit profile

--- src/react/components/EditProfile/EditProfile.js
+++ src/react/components/EditProfile/EditProfile.js
@@ -1,16 +1,60 @@
 import React from "react";
 import ImageUpload from "./ImageUpload";
-import { Card, Button, Input } from "antd";
+import { Modal, Button, Input } from "antd";
 import "antd/dist/antd.css";
 import "./EditProfile.css";
 
 const { TextArea } = Input;
 
 class EditProfile extends React.Component {
+  state = {
+    loading: false,
+    visible: false
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  handleOk = () => {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
   render() {
+    const { visible, loading } = this.state;
     return (
-      <div className="container1">
-        <Card className="card">
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          Edit Profile
+        </Button>
+        <Modal
+          visible={visible}
+          title="Title"
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={[
+            <Button key="back" onClick={this.handleCancel}>
+              Return
+            </Button>,
+            <Button
+              key="submit"
+              type="primary"
+              loading={loading}
+              onClick={this.handleOk}
+            >
+              Submit
+            </Button>
+          ]}
+        >
           <span>
             <ImageUpload />
             <div style={{ textAlign: "center" }}>
@@ -23,18 +67,17 @@ class EditProfile extends React.Component {
           <br />
           <Input placeholder="Location" />{" "}
           <div className="row container">
-              <Button className="button insideButton" type="danger" ghost>
-                Delete Profile
-              </Button>
-              <Button className="button insideButton" type="primary" ghost>
-                Update
-              </Button>
+            <Button className="insideButton" type="danger" ghost>
+              Delete Profile
+            </Button>
+            <Button className="insideButton" type="primary" ghost>
+              Update
+            </Button>
           </div>
-        </Card>
+        </Modal>
       </div>
     );
   }
 }
 
 export default EditProfile;
-
--- src/react/pages/Profile.js
+++ src/react/pages/Profile.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {Menu} from "../components";
+import { Menu } from "../components";
 import { userIsAuthenticated } from "../HOCs";
-import EditProfile from "../components/EditProfile/EditProfile";
-
+// import EditProfile from "../components/EditProfile/EditProfile";
+// import ListOfUsers from "../components/ListOfUsers/ListOfUsers";
+import { UserProfile } from '../components'
 
 class Profile extends React.Component {
-
-
   render() {
     return (
       <>
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
-        <EditProfile />
+        <UserProfile/>
+        {/* <ListOfUsers /> */}
       </>
     );
   }
