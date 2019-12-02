90f96d4dcaa8ee38c75c2c8ece93d4719463bb55
Chelsea <chewh115@gmail.com>
2019-12-02 11:20:10 -0500

    Updates signup page to be formatted correctly.

--- src/react/pages/Signup.js
+++ src/react/pages/Signup.js
@@ -4,11 +4,11 @@ class Signup extends React.Component {
     render(){
         return(
             <React.Fragment>
-            <input type="username" placeholder="Enter Username"></input>
-            <input type="displayname" placeholder="Enter displayname"></input>
-            <input type="password" placeholder="Password"></input>
-            <input type="password" placeholder="Confirm Password"></input>
-            <button type="Confirm">Sign Up Now!</button>
+                <input type="username" placeholder="Enter Username"></input>
+                <input type="displayname" placeholder="Enter displayname"></input>
+                <input type="password" placeholder="Password"></input>
+                <input type="password" placeholder="Confirm Password"></input>
+                <button type="Confirm">Sign Up Now!</button>
             </React.Fragment>
         )
     }
