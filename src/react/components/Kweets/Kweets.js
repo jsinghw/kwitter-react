2b67d42fa32a754a63f36e60b0016bbda5ee4e86
Detrich Schilling <detrich2k@gmail.com>
2019-12-02 14:05:05 -0500

    updated kweets to have actual information from the api

--- src/react/components/Kweets/Kweets.js
+++ src/react/components/Kweets/Kweets.js
@@ -2,77 +2,8 @@ import { Comment, Icon, Tooltip, Avatar,Card } from 'antd';
 import moment from 'moment';
 import React from "react"
 import "../KweetCard/KweetCard.css"
-const messages= [
-  {
-    "id": 995,
-    "text": "This is another kweet",
-    "username": "test",
-    "createdAt": "2019-11-21T17:04:56.183Z",
-    "likes": []
-  },
-  {
-    "id": 994,
-    "text": "This is a lunch kweet.",
-    "username": "test",
-    "createdAt": "2019-11-21T17:04:11.921Z",
-    "likes": []
-  },
-  {
-    "id": 993,
-    "text": "test",
-    "username": "test",
-    "createdAt": "2019-11-21T16:49:41.258Z",
-    "likes": []
-  },
-  {
-    "id": 992,
-    "text": "heyyyyyy",
-    "username": "test",
-    "createdAt": "2019-11-21T15:03:34.112Z",
-    "likes": []
-  },
-  {
-    "id": 991,
-    "text": "Heyy\n",
-    "username": "test",
-    "createdAt": "2019-11-21T15:03:18.633Z",
-    "likes": []
-  },
-  {
-    "id": 990,
-    "text": "Yayyyyy.",
-    "username": "test",
-    "createdAt": "2019-11-21T14:48:44.104Z",
-    "likes": []
-  },
-  {
-    "id": 989,
-    "text": "hey!!!!!!!!!",
-    "username": "test",
-    "createdAt": "2019-11-21T14:48:20.417Z",
-    "likes": []
-  },
-  {
-    "id": 988,
-    "text": "Hey!",
-    "username": "test",
-    "createdAt": "2019-11-21T14:08:37.411Z",
-    "likes": []
-  },
-  {
-    "id": 987,
-    "text": "Hello Morning",
-    "username": "test",
-    "createdAt": "2019-11-21T14:00:54.302Z",
-    "likes": []
-  },
-  {
-    "id": 986,
-    "text": "It might be!",
-    "username": "test",
-    "createdAt": "2019-11-21T12:33:43.636Z",
-    "likes": []
-  }]
+import { withAsyncAction } from "../../HOCs";
+import {Spinner} from "..";
 
 class Kweets extends React.Component {
   state = {
@@ -96,9 +27,16 @@ class Kweets extends React.Component {
       action: 'disliked',
     });
   };
+  componentDidMount() {
+    this.props.getUserMessages();
+  }
 
   render() {
-    return messages.map(message => {
+    if (this.props.result === null) {
+      return <div><Spinner name="circle" color="blue" /></div>
+    }
+    const getMessages = this.props.result.messages;
+    return getMessages.map(message => {
     const { likes, dislikes, action } = this.state;
 
     const actions = [
@@ -122,7 +60,6 @@ class Kweets extends React.Component {
         </Tooltip>
         <span style={{ paddingLeft: 8, cursor: 'auto' }}>{dislikes}</span>
       </span>,
-      <span key="comment-basic-reply-to">Reply to</span>,
     ];
     
     return (
@@ -147,7 +84,7 @@ class Kweets extends React.Component {
         }
         datetime={
           <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-            <span>{moment().fromNow()}</span>
+            <span>{moment(message.createdAt).fromNow()}</span>
           </Tooltip>
         }
       />
@@ -158,4 +95,4 @@ class Kweets extends React.Component {
   }
     )}}
 
-export default Kweets;
+export default  withAsyncAction("usermessages", "getUserMessages")(Kweets);
