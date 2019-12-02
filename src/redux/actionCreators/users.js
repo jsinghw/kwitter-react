2e369cde590ea053e4b68979a03a72060f800afd
Chelsea <chewh115@gmail.com>
2019-12-02 18:21:57 -0500

    Fills out majority of createUser action creator

--- src/redux/actionCreators/users.js
+++ src/redux/actionCreators/users.js
@@ -1,2 +1,30 @@
 import { domain, jsonHeaders, handleJsonResponse } from "./constants";
 import { CREATEUSER } from "../actionTypes";
+
+const url = domain + "/users";
+
+export const createUser = () => (dispatch, getState) => {
+    dispatch({
+        type: CREATEUSER.START
+    });
+
+    return fetch(url, {
+        method: "POST",
+        headers: jsonHeaders
+    })
+        .then(handleJsonResponse)
+        .then(result => {
+            return dispatch({
+                type: CREATEUSER.SUCCESS,
+                payload: result
+            });
+        })
+        .catch(err => {
+            return Promise.reject(
+                dispatch({
+                    type: CREATEUSER.FAIL,
+                    payload: err.message
+                })
+            );
+        });
+};
