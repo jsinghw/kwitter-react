2b1aafd93bf669e440c04406e9566658eb56e25f
Chelsea <chewh115@gmail.com>
2019-12-05 11:23:57 -0500

    Having to commit to switch branch to checkout master page

--- src/react/components/SignupForm/SignupForm.js
+++ src/react/components/SignupForm/SignupForm.js
@@ -3,12 +3,86 @@ import "./SignupForm.css";
 import { Modal, Form, Input, Tooltip, Icon } from "antd";
 import { withAsyncAction } from "../../HOCs";
 
+const SignUpForm = Form.create({
+  name: 'global_state',
+  onFieldsChange(props, changedFields) {
+    props.onChange(changedFields);
+  },
+  mapPropsToFields(props) {
+    return {
+      username: Form.createFormField({
+        ...props.username,
+        value: props.username.value,
+      }),
+      displayName: Form.createFormField({
+        ...props.displayName,
+        value: props.displayName.value,
+      }),
+      password: Form.createFormField({
+        ...props.password,
+        value: props.password.value,
+      }),
+    };
+  },
+  onValuesChange(_, values) {
+    console.log(values);
+  },
+})(props => {
+  const { getFieldDecorator } = props.form;
+  return (
+    <Form layout="vertical">
+      <Form.Item label="Username, 3-20 Characters">
+        {getFieldDecorator('username', {
+          rules: [{ required: true, message: 'Username is required!' }],
+        })(<Input placeholder="username" type="textarea" />)}
+      </Form.Item>
+      <Form.Item name="displayName" label={
+        <span>
+          Display Name, 3-20 characters&nbsp;
+          <Tooltip title="What do you want your name to appear as?">
+            <Icon type="question-circle-o" />
+          </Tooltip>
+        </span> 
+      }>
+        {getFieldDecorator("display name", {
+          rules: [{ required: true, message: "Please input a display name!" }]
+        })(<Input placeholder="Display Name" type="textarea" />)}
+      </Form.Item>
+      <Form.Item name="password" label="Password, 3-20 characters">
+        {getFieldDecorator("password", {
+          rules: [
+            { 
+              required: true, message: "Please input a password!" },
+          ]
+        })(<Input placeholder="Password" type="password" />)}
+      </Form.Item>
+      <Form.Item label="Confirm Password">
+        {getFieldDecorator("confirm password", {
+          rules: [
+            { 
+              required: true, message: "Please confirm your password!" 
+            },
+          ]
+        })(<Input placeholder="Confirm Password" type="password" />)}
+      </Form.Item>
+    </Form>
+  );
+});
+
 class Signup extends React.Component {
   state = {
     visible: false,
-    username: "",
-    displayName: "",
-    password: ""
+    fields: {
+      username: {
+        value: ""
+      },
+      displayName: {
+        value: ""
+      },
+      password: {
+        value: ""
+      }
+    }
   };
 
   showModal = () => {
@@ -19,25 +93,10 @@ class Signup extends React.Component {
     this.setState({ visible: false });
   };
 
-  handleCreate = e => {
-    const { form } = this.formRef.props;
-    form.validateFields((err, values) => {
-      if (err) {
-        return;
-      }
-
-      e.preventDefault();
-      const { username, displayName, password } = this.state
-      this.props.createUser(username, displayName, password);
-
-      console.log("Received values of form: ", values);
-      form.resetFields();
-      this.setState({ visible: false });
-    });
-  };
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  handleFormChange = changedFields => {
+    this.setState(({ fields }) => ({
+      fields: { ...fields, ...changedFields },
+    }));
   };
 
   saveFormRef = formRef => {
@@ -47,6 +106,7 @@ class Signup extends React.Component {
   render() {
     const { visible, form } = this.props;
     const { getFieldDecorator } = form;
+    const { fields } =
     return (
       <div className="signUpButton">
         <button type="button" onClick={this.showModal}>
@@ -57,44 +117,9 @@ class Signup extends React.Component {
           title="Create a new account"
           okText="Create"
           onCancel={this.handleCancel}
-          onOk={this.handleCreate}
+          onOk={this.handleFormChange}
         >
-          <Form layout="vertical">
-            <Form.Item name="username" label="Username, 3-20 characters">
-              {getFieldDecorator("username", {
-                rules: [{ required: true, message: "Please input a username!" }]
-              })(<Input placeholder="Username" />)}
-            </Form.Item>
-            <Form.Item name="displayName" label={
-              <span>
-                Display Name, 3-20 characters&nbsp;
-                <Tooltip title="What do you want your name to appear as?">
-                  <Icon type="question-circle-o" />
-                </Tooltip>
-              </span> 
-            }>
-              {getFieldDecorator("display name", {
-                rules: [{ required: true, message: "Please input a display name!" }]
-              })(<Input placeholder="Display Name" type="textarea" />)}
-            </Form.Item>
-            <Form.Item name="password" label="Password, 3-20 characters">
-              {getFieldDecorator("password", {
-                rules: [
-                  { 
-                    required: true, message: "Please input a password!" },
-                ]
-              })(<Input placeholder="Password" type="password" />)}
-            </Form.Item>
-            <Form.Item label="Confirm Password">
-              {getFieldDecorator("confirm password", {
-                rules: [
-                  { 
-                    required: true, message: "Please confirm your password!" 
-                  },
-                ]
-              })(<Input placeholder="Confirm Password" type="password" />)}
-            </Form.Item>
-          </Form>
+
         </Modal>
       </div>
     );
