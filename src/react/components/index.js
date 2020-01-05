11938eb8e07fc34ac54568c7733108eaf1be5566
Chelsea <chewh115@gmail.com>
2020-01-05 15:46:50 -0500

    Removes one final component that wasn't being used and updates exports on components index.js to reflect

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -6,7 +6,6 @@ export { default as UserProfile } from "./UserProfile/UserProfile";
 export { default as KweetList } from "./KweetList";
 export { default as MessageList } from "./MessageList/MessageList";
 export { default as ListOfUsers } from "./ListOfUsers/ListOfUsers";
-export { default as MessageCard } from "./MessageCard/MessageCard";
 export { default as ProfileMessages } from "./ProfileMessages/ProfileMessages";
 export { default as DeleteUserButton } from "./DeleteUserButton/DeleteUserButton";
 export { default as DeleteMessageButton } from "./DeleteMessageButton/DeleteMessageButton";
